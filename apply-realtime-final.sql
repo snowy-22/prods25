b01034d1b172b4b1e989ab8cf71a598cf192f004
Doruk Karlikaya <dorukkarlikaya@gmail.com>
2026-01-26 22:38:59 +0300

    fix: Force clean npm install in Vercel to bypass cache issue + add realtime migration scripts

---
+++ apply-realtime-final.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ec08b28eddea14c467dc77dd391df595e02c825c2290db84a33bade616210c3
+size 2039
---
+++ apply-realtime-migration-safe.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1cdfb1160d43febb0e07f6503dc0504edc2e1e0ebd7391ef0e7c9fa935dadbe7
+size 5730
