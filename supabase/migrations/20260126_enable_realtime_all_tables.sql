9d41b1bb16ffb1a8498f8850579fcbce9fe22a73
Doruk Karlikaya <dorukkarlikaya@gmail.com>
2026-01-26 22:04:57 +0300

    fix: Remove IF NOT EXISTS from ALTER PUBLICATION (invalid PostgreSQL syntax)

--- supabase/migrations/20260126_enable_realtime_all_tables.sql
+++ supabase/migrations/20260126_enable_realtime_all_tables.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24b281988b5849afc2cb78a45bcc78dc472085c142c9ae3a0379a423dc69b5f7
-size 7040
+oid sha256:81c8d8daac70d618ecbd9e692c3d58a23f66be94e07f3092404891eb034d43d9
+size 6228
